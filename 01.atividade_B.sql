f0d0a863e386614a9f26dface3d0fa3328c62b95
Pedro Alves <pedroalves200510@gmail.com>
2024-06-25 17:50:10 -0300

    Add files via upload

---
+++ 01.atividade_B.sql
@@ -0,0 +1,15 @@
+/*04. Detalha/Exibe a estrutura da tabela*/
+describe cliente;
+describe compra;
+describe funcionario;
+describe departamento;
+describe fornecedor;
+describe produto;
+
+/*05. Consulta todos os registros da tabela*/
+select * from cliente;
+select * from compra;
+select * from funcionario;
+select * from departamento;
+select * from fornecedor;
+select * from produto;
