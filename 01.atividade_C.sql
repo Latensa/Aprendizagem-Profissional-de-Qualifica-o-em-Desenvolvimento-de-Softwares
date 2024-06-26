9e452a48649fad2edc4576a8d00396861058bc48
Pedro Alves <pedroalves200510@gmail.com>
2024-06-26 16:41:20 -0300

    Add files via upload

---
+++ 01.atividade_C.sql
@@ -0,0 +1,6 @@
+insert into cliente(codigo, nome, endereco, cpf, telefone) values(333, 'Jose da Silva', 'rua leopoldo', 99988877766, 99888888888)
+insert into compra(codigo, quantidade_produtos) values(666, 12)
+insert into funcionario(codigo ,nome ,cpf ,telefone) values(777, 'Pedro de Algo', 09095409321, 61996713665) 
+insert into departamento(codigo, descrição) values(999, 'Lorem ipsum dolor sit amet')
+insert into fornecedor(codigo, nome ,telefone) values(555, 'Anderson Silva', 61887645772)
+insert into produto(codigo, descrição, valor) values(444, 'Lorem ipsum dolor sit amet', 320)
